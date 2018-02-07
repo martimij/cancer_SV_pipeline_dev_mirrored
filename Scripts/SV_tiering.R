4fd7bfdee42dae405fcef9a7e212a33176e9ecaa
Martina Mijuskovic <martina.mijuskovic@genomicsengland.co.uk>
2018-02-07 18:07:21 +0000

    REF/ALT extracted, directionality flags added

--- Scripts/SV_tiering.R
+++ Scripts/SV_tiering.R
@@ -895,15 +895,248 @@ domain1_SVs_fix4 <- domain1_SVs_fix4 %>% dplyr::select(-(CSQR), -(CSQT), -(phylo
 dim(domain1_SVs_fix1)  # 10
 dim(domain1_SVs_fix2)  # 88
 dim(domain1_SVs_fix3)  # 422
-dim(domain1_SVs_fix4)
+dim(domain1_SVs_fix4)  # 502
+
+# Merge all SVs
+domain1_SVs_fix <- rbind(domain1_SVs_fix1, domain1_SVs_fix2, domain1_SVs_fix3, domain1_SVs_fix4)
+rm(domain1_SVs_fix1, domain1_SVs_fix2, domain1_SVs_fix3, domain1_SVs_fix4)
+
+# Summary ((NOTE that any DEL >10kb is automatically filtered out and not included here)
+dim(domain1_SVs_fix)  # 1022 total (TRs listed as 2 BNDs)
+table(domain1_SVs_fix$SVTYPE)  # 719 Domain 1 SVs (~1 per patient)
+# BND DEL DUP INS INV 
+# 606 128  27  18 243
+
+# Proportion of different SV types
+table(domain1_SVs_fix$SVTYPE)/719
+# BND        DEL        DUP        INS        INV 
+# 0.84283727 0.17802503 0.03755216 0.02503477 0.33796940  # transl are 0.4214186
+
+# Median SV per patient
+median(as.numeric(table(domain1_SVs_fix$SAMPLE_WELL_ID)))  # 2
+
+
+# Number of patients with Domain 1 SVs
+length(unique(domain1_SVs_fix$SAMPLE_WELL_ID))  # 311/759 (41% patients have Domain 1 SVs)
+table(domain1_SVs_fix$SVTYPE, domain1_SVs_fix$Application)  # No Canvas
+table(domain1_SVs_fix$FILTER, domain1_SVs_fix$SOMATIC)
+table(domain1_SVs_fix$SVTYPE, domain1_SVs_fix$IMPRECISE)
+# FALSE TRUE
+# BND   464  142
+# DEL   127    1
+# DUP    27    0
+# INS    18    0
+# INV   201   42
+domain1_SVs_fix %>% dplyr::select(SAMPLE_WELL_ID, ID, IMPRECISE, SVTYPE, SVLEN, CHR, START, END, ColocalizedCanvas, PR_ALT, SR_ALT, start_ann, end_ann)
+
+
+# Add KEY, calculate recurrent variants, add VAF and variant frequency info to the table
+domain1_SVs_fix$KEY <- sapply(1:dim(domain1_SVs_fix)[1], function(x){
+  paste(domain1_SVs_fix$CHR[x], domain1_SVs_fix$START[x], domain1_SVs_fix$END[x], domain1_SVs_fix$SVTYPE[x], sep = "-")
+})
+
+unique_keys <- unique(domain1_SVs_fix$KEY)
+length(unique_keys)  # 1004
+recurrent_SVs <- data.frame(
+  KEY = unique_keys,
+  NUM_OBS = sapply(unique_keys, function(x){ sum(domain1_SVs$KEY == x)})
+)
+rownames(recurrent_SVs) <- NULL
+# Look at recurrent SVs
+recurrent_SVs %>% filter(NUM_OBS > 1) %>% arrange(desc(NUM_OBS))
+
+# Add variant frequency info to the main table
+recurr_keys <- as.character(recurrent_SVs %>% filter(NUM_OBS >1) %>% pull(KEY))
+domain1_SVs_fix$recurrent <- 0  
+domain1_SVs_fix[domain1_SVs_fix$KEY %in% recurr_keys,]$recurrent <- 1  # 9 total (3 deletions, 3 transl)
+table(domain1_SVs_fix$recurrent, domain1_SVs_fix$SVTYPE)
+domain1_SVs_fix %>% filter(recurrent == 1, SVTYPE == "DEL")
+domain1_SVs_fix %>% filter(recurrent == 1, KEY == "4-54688371-54688815-DEL")
+domain1_SVs_fix %>% filter(recurrent == 1, KEY == "12-68810707-68811111-DEL")
+domain1_SVs_fix %>% filter(recurrent == 1, KEY == "5-142596308-142596582-DEL")
+domain1_SVs_fix %>% filter(recurrent == 1, SVTYPE == "BND")
+domain1_SVs_fix %>% filter(recurrent == 1, KEY == "10-43115587-NA-BND")
+domain1_SVs_fix %>% filter(recurrent == 1, ID == "MantaBND:114898:0:1:0:0:0:0") # 2 SVs in same EVENT, sharing one breakopoint, but mate not recurrent
+domain1_SVs_fix %>% filter(recurrent == 1, ID == "MantaBND:114898:0:1:1:3:0:1") # 2 SVs in same EVENT, sharing one breakopoint, but mate not recurrent
+
+### Remove BND with missing mates and recalculate summaries
+domain1_SVs_fix$missing_mate <- 0
+missing_mates <- sapply(unique(domain1_SVs_fix$SAMPLE_WELL_ID), function(x){
+  mates <- domain1_SVs_fix %>% filter(SAMPLE_WELL_ID == x, SVTYPE == "BND") %>% pull(MATEID)
+  # List missing mates
+  mates[!mates %in% (domain1_SVs_fix %>% filter(SAMPLE_WELL_ID == x, SVTYPE == "BND") %>% pull(ID))]
+})
+table(as.character(missing_mates))  # none missing
+
+
+### Calculate VAF
+domain1_SVs_fix$VAF <- sapply(1:dim(domain1_SVs_fix)[1], function(x){
+  sum(domain1_SVs_fix$PR_ALT[x], domain1_SVs_fix$SR_ALT[x], na.rm = T)/sum(domain1_SVs_fix$PR_REF[x], domain1_SVs_fix$SR_REF[x], domain1_SVs_fix$PR_ALT[x], domain1_SVs_fix$SR_ALT[x], na.rm = T)
+})
+
+# Mean VAF by SV type
+domain1_SVs_fix %>% group_by(SVTYPE) %>% summarise(mean(VAF), median(VAF))
+
+
+# Unlist variables
+domain1_SVs_fix$SVINSLEN <- as.numeric(domain1_SVs_fix$SVINSLEN)
+domain1_SVs_fix$SVLEN <- as.numeric(domain1_SVs_fix$SVLEN)
+domain1_SVs_fix$CIGAR <- as.character(domain1_SVs_fix$CIGAR)
+# domain1_SVs_fix$CIPOS <- as.character(domain1_SVs_fix$CIPOS)   # info loss
+# domain1_SVs_fix$CIEND <- as.character(domain1_SVs_fix$CIEND)   # info loss
+domain1_SVs_fix$HOMLEN <- as.numeric(domain1_SVs_fix$HOMLEN)
+domain1_SVs_fix$HOMSEQ <- as.character(domain1_SVs_fix$HOMSEQ)
+domain1_SVs_fix$SVINSSEQ <- as.character(domain1_SVs_fix$SVINSSEQ)
+domain1_SVs_fix$LEFT_SVINSSEQ <- as.character(domain1_SVs_fix$LEFT_SVINSSEQ)
+domain1_SVs_fix$RIGHT_SVINSSEQ <- as.character(domain1_SVs_fix$RIGHT_SVINSSEQ)
 
 
 
-#### Repeat annotation #### 
+#### Add directionality flags ####
+
+# For INV, directionality flags already exist in the table (2 possibilities: INV3, INV5)
+# For BND, directionality flags need to be derived from the ALT structure (is relevant region upstream or downstream of POS?)
+
+# Add BND direction flag (equivalent to INV, BND3=chr upstream of POS is included, BND5=chr downstream of POS is included in translocation)
+domain1_SVs_fix$BND5 <- grepl("^]|^\\[", domain1_SVs_fix$ALT)   # ALT starts with brackets ([ or ]), indicating that POS is the 5' end of transclocation junction
+domain1_SVs_fix$BND3 <- (grepl("]|\\[", domain1_SVs_fix$ALT) & !grepl("^]|^\\[", domain1_SVs_fix$ALT))  # ALT has brackets, but doesn't start with them
+# Sanity check
+head((domain1_SVs_fix %>% filter(SVTYPE == "BND") %>% dplyr::select(KEY, ALT, BND3, BND5)),30)
+head((domain1_SVs_fix %>% filter(SVTYPE != "BND") %>% dplyr::select(KEY, ALT, BND3, BND5)),30)
+
+
+
+#### Mappablity and repeat overlap ####
 
 # Average fragment size for FF PCR-free
 mean((QC %>% filter(LIBRARY_TYPE == "TruSeq PCR-Free") %>% pull(AV_FRAGMENT_SIZE_BP)), na.rm = T) # 483.1857
 median((QC %>% filter(LIBRARY_TYPE == "TruSeq PCR-Free") %>% pull(AV_FRAGMENT_SIZE_BP)), na.rm = T)  # 482.3
+
+# Assess IMPRECISE flag against SR support
+table(domain1_SVs_fix$IMPRECISE, domain1_SVs_fix$SR_ALT == 0, exclude = NULL)  # imprecise have SR_ALT = NA; some precise have SR_ALT = 0
+#           FALSE TRUE <NA>
+# FALSE     833    4    0
+# TRUE      0      0   185
+# <NA>      0      0    0
+domain1_SVs_fix %>% filter(IMPRECISE == FALSE, SR_ALT == 0) %>% dplyr::select(IMPRECISE, SVTYPE, CIPOS, ID, MATEID, HOMLEN, ALT, KEY, PR_REF, PR_ALT, SR_REF, SR_ALT, SAMPLE_WELL_ID)
+
+
+# Create a bed file with SVs, start and end separately, remove NAs
+# Note that for BED format START has to be adjusted to 0-based (-1 bp) and END position is not included (stays same)
+# Add 240 bp to the breakpoint (region defined based on SV type and directionality)
+
+start_bed <- domain1_SVs_fix %>% mutate(
+  START_bed = case_when(
+    SVTYPE == "DEL" ~ START-240,
+    SVTYPE == "INS" ~ START-240
+    SVTYPE == "DUP" ~ START,
+    SVTYPE == "INV" & INV3 == TRUE ~ START-240,
+    SVTYPE == "INV" & INV5 == TRUE ~ START,
+    SVTYPE == "BND" & BND3 == TRUE ~ START-240,
+    SVTYPE == "BND" & BND5 == TRUE ~ START
+    ),
+  END_bed = case_when(
+    SVTYPE == "DEL" ~ START,
+    SVTYPE == "INS" ~ START,
+    SVTYPE == "DUP" ~ START+240,
+    SVTYPE == "INV" & INV3 == TRUE ~ START,
+    SVTYPE == "INV" & INV5 == TRUE ~ START+240,
+    SVTYPE == "BND" & BND3 == TRUE ~ START,
+    SVTYPE == "BND" & BND5 == TRUE ~ START+240
+  )
+) %>% dplyr::select(CHR, START_bed, END_bed, KEY)
+
+# Extend regions for CIPOS and CIEND or replace with START+CIPOS[1] to START+CIPOS[2] (same for END) if IMPRECISE
+
+
+
+start_bed <- cbind((ff_ffpe_merged %>% dplyr::select(CHR, START)), (ff_ffpe_merged %>% dplyr::select(START, KEY, Type2)))
+names(start_bed)[3] <- "end"
+start_bed$Score <- ""
+start_bed <- start_bed %>% dplyr::select(CHR, START, end, KEY, Score, Type2)
+start_bed <- start_bed %>% filter(!is.na(START))
+# Adjust window around breaksite
+start_bed$START <- start_bed$START - 151
+start_bed$end <- start_bed$end + 150
+write.table(start_bed, file = paste0(patientID, "_sv_start.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
+
+end_bed <- cbind((ff_ffpe_merged %>% dplyr::select(CHR, END)), (ff_ffpe_merged %>% dplyr::select(END, KEY, Type2)))
+names(end_bed)[2] <- "start"
+end_bed$Score <- ""
+end_bed <- end_bed %>% dplyr::select(CHR, start, END, KEY, Score, Type2)
+end_bed <- end_bed %>% filter(!is.na(END))
+# Adjust window around breaksite
+end_bed$start <- end_bed$start - 151
+end_bed$END <- end_bed$END + 150
+write.table(end_bed, file = paste0(patientID, "_sv_end.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
+
+
+
+
+
+### Call bedtools to find overlaps with WindowMasker 
+
+# start
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_start.bed"), "-b /home/mmijuskovic/FFPE/windowmaskerSdust.hg38.bed >", paste0(patientID, "_sv_wMasker_start_overlap.bed")), intern = T)
+sv_wMasker_start <- read.table(paste0(patientID, "_sv_wMasker_start_overlap.bed"), sep = "\t")
+names(sv_wMasker_start) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+# end
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_end.bed"), "-b /home/mmijuskovic/FFPE/windowmaskerSdust.hg38.bed >", paste0(patientID, "_sv_wMasker_end_overlap.bed")), intern = T)
+sv_wMasker_end <- read.table(paste0(patientID, "_sv_wMasker_end_overlap.bed"), sep = "\t")
+names(sv_wMasker_end) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+
+# FLAG SVs where START or END overlaps with WindowMasker
+wMasker_keys <- unique(c(as.character(sv_wMasker_start %>% filter(NumOverlap != 0) %>% .$KEY), as.character(sv_wMasker_end %>% filter(NumOverlap != 0) %>% .$KEY)))
+ff_ffpe_merged$wMasker_filtered <- 0
+ff_ffpe_merged[(ff_ffpe_merged$KEY %in% wMasker_keys),]$wMasker_filtered <- 1
+
+
+### Call bedtools to find overlaps with simple repeats
+
+# start
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_start.bed"), "-b /home/mmijuskovic/FFPE/simpleRepeat.hg38.bed >", paste0(patientID, "_sv_repeats_start_overlap.bed")), intern = T)
+sv_repeats_start <- read.table(paste0(patientID, "_sv_repeats_start_overlap.bed"), sep = "\t")
+names(sv_repeats_start) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+# end
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_end.bed"), "-b /home/mmijuskovic/FFPE/simpleRepeat.hg38.bed >", paste0(patientID, "_sv_repeats_end_overlap.bed")), intern = T)
+sv_repeats_end <- read.table(paste0(patientID, "_sv_repeats_end_overlap.bed"), sep = "\t")
+names(sv_repeats_end) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+
+# FLAG SVs where START or END overlaps with repeats
+repeats_keys <- unique(c(as.character(sv_repeats_start %>% filter(NumOverlap != 0) %>% .$KEY), as.character(sv_repeats_end %>% filter(NumOverlap != 0) %>% .$KEY)))
+ff_ffpe_merged$repeats_filtered <- 0
+ff_ffpe_merged[(ff_ffpe_merged$KEY %in% repeats_keys),]$repeats_filtered <- 1
+
+
+
+### Call bedtools to find overlaps with segmental duplications
+
+# start
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_start.bed"), "-b /home/mmijuskovic/FFPE/genomicSuperDups.hg38.bed >", paste0(patientID, "_sv_segdups_start_overlap.bed")), intern = T)
+sv_segdups_start <- read.table(paste0(patientID, "_sv_segdups_start_overlap.bed"), sep = "\t")
+names(sv_segdups_start) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+# end
+system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -a", paste0(patientID, "_sv_end.bed"), "-b /home/mmijuskovic/FFPE/genomicSuperDups.hg38.bed >", paste0(patientID, "_sv_segdups_end_overlap.bed")), intern = T)
+sv_segdups_end <- read.table(paste0(patientID, "_sv_segdups_end_overlap.bed"), sep = "\t")
+names(sv_segdups_end) <- c("CHR", "START", "END", "KEY", "Score", "Type2", "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+
+# FLAG SVs where START or END overlaps with repeats
+segdups_keys <- unique(c(as.character(sv_segdups_start %>% filter(NumOverlap != 0) %>% .$KEY), as.character(sv_segdups_end %>% filter(NumOverlap != 0) %>% .$KEY)))
+ff_ffpe_merged$segdups_filtered <- 0
+ff_ffpe_merged[(ff_ffpe_merged$KEY %in% segdups_keys),]$segdups_filtered <- 1 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ### Step 1: overlap SV breakpoints with Umap36
@@ -911,7 +1144,6 @@ median((QC %>% filter(LIBRARY_TYPE == "TruSeq PCR-Free") %>% pull(AV_FRAGMENT_SI
 # Load Umap M36 bedgraph, only 100% mappable regions (others filtered out)
 umap36 <- read.table("./Data/mappability_repeats/mappable_k36.umap.bedgraph", skip = 1, col.names = c("CHR", "START", "END", "umap36"))
 
-# Get overlap of pre- and post- START and END positions for all SVs
 
 
 
